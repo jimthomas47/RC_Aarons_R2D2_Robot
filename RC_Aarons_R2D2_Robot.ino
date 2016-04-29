88b0a4375fb3b1f7fe7b00c3fbcd588186edac83
Cory Thomas <dump247@users.noreply.github.com>
2016-04-29 11:03:15 -0500

    Removed unused code

--- RC_Aarons_R2D2_Robot.ino
+++ RC_Aarons_R2D2_Robot.ino
@@ -92,74 +92,23 @@ const int HmotorPinR = 11; // R2D2 head turn
 // lookup motorPower = [j_RUD][j_RLR]
 // 6x6 version
 const int LmotorPower [7][7] = {
-  { -255, -255, -255, -255, -200, -150, 0},
-  { -255, -200, -200, -200, -150, 0, 150},
-  { -255, -200, -150, -150, 0, 150, 200},
-  { -255, -200, -150, 0, 150, 200, 255},
-  { -200, -150, 0, 150, 150, 200, 255},
-  { -100, 0, 100, 200, 200, 200, 255},
-  {0, 150, 200, 255, 255, 255, 255}
+  { -255, -255, -255, -255, -200, -150,   0 },
+  { -255, -200, -200, -200, -150,    0, 150 },
+  { -255, -200, -150, -150,    0,  150, 200 },
+  { -255, -200, -150,    0,  150,  200, 255 },
+  { -200, -150,    0,  150,  150,  200, 255 },
+  { -100,    0,  100,  200,  200,  200, 255 },
+  {    0,  150,  200,  255,  255,  255, 255 }
 };
 const int RmotorPower [7][7] = {
-  {0, -150, -200, -255, -255, -255, -255},
-  {150, 0, -150, -200, -200, -200, -255},
-  {200, 150, 0, -150, -150, -200, -255},
-  {255, 200, 150, 0, -150, -200, -255},
-  {255, 200, 150, 150, 0, -150, -200},
-  {255, 200, 200, 200, 150, 0, -150},
-  {255, 255, 255, 255, 200, 150, 0}
-
+  {   0, -150, -200, -255, -255, -255, -255 },
+  { 150,    0, -150, -200, -200, -200, -255 },
+  { 200,  150,    0, -150, -150, -200, -255 },
+  { 255,  200,  150,    0, -150, -200, -255 },
+  { 255,  200,  150,  150,    0, -150, -200 },
+  { 255,  200,  200,  200,  150,    0, -150 },
+  { 255,  255,  255,  255,  200,  150,    0 }
 };
-
-// output pin definitions - to robot head
-const int SpinMotor  = 28;  // blk - motor that puts spin on disc / red Vbatt
-const int ShootMotor = 32;  // blue - motor that ejects disc / orange Vbatt
-const int LeftEyeLed = 29;  // yellow - left eye led / gnd = black
-const int RightEyeLed = 33; // red - right eye led / gnd = black
-
-const int SpeakerPin = 6;   // speaker output pin
-
-const int SpotPin = 7;  // spotlight output
-const int SpotServo  = 12;  // small servo motor for LED spotlight
-int SpotAngle = 0;  // spotlight servo angle in degrees
-boolean SpotButton = false; // for toggle of spotlight
-boolean SpotOn = false; // spotlight on state
-Servo servo1;  // Spotlight servo
-
-const int ArmServo = 13;  // large servo motor for Arm up/down
-Servo servo2;  // Arm servo
-int ArmAngle = 90;  // current arm angle.. constrain to 0-180
-int ArmIncrement = 1;  // # degrees to increment servo angle
-
-const int RotPin = 36; // blue rotating light pin (transistor drive)
-boolean RotButton = false;
-boolean RotOn = false;
-
-boolean EyesButton = false; // robot eyes
-boolean EyesOn  = false;
-
-// LED's in robot's hand
-const int HandLED1 = 35;
-const int HandLED2 = 37;
-const int HandLED3 = 39;
-int LaserTone = 60; // variable tone for hand laser LEDs
-int count = 0;
-int lcount = 0;
-
-// pins to control WTV020-SD-16P uSD Audio module in Wtv020sd16p
-const int resetPin = 26;
-const int clockPin = 27;
-const int dataPin = 30;
-const int busyPin = 31;
-
-
-/*
-  Create an instance of the Wtv020sd16p class.
-  1st parameter: Reset pin number.
-  2nd parameter: Clock pin number.
-  3rd parameter: Data pin number.
-  4th parameter: Busy pin number.
-*/
 
 void setup(void) {
   Serial.begin(9600);
@@ -187,28 +136,6 @@ void setup(void) {
   mp3.setVolume(25);
   mp3.setLoopMode(MP3_LOOP_NONE);
 
-  // Robot head
-  pinMode (SpinMotor, OUTPUT);
-  pinMode (ShootMotor, OUTPUT);
-  pinMode (LeftEyeLed, OUTPUT);
-  pinMode (RightEyeLed, OUTPUT);
-  digitalWrite(SpinMotor, LOW);
-  digitalWrite(ShootMotor, LOW);
-  digitalWrite(LeftEyeLed, LOW);
-  digitalWrite(RightEyeLed, LOW);
-
-  pinMode (SpeakerPin, OUTPUT);
-  pinMode (SpotPin, OUTPUT);
-  pinMode (RotPin, OUTPUT);
-  pinMode (HandLED1, OUTPUT);
-  pinMode (HandLED2, OUTPUT);
-  pinMode (HandLED3, OUTPUT);
-
-
-  servo1.attach(SpotServo);
-  servo2.attach(ArmServo);
-  servo2.write(ArmAngle);   // initialize to 90
-
   // start the radio
   radio.begin();
   radio.setChannel(0x4F); // change from default setting (0x4C) to avoid interference
@@ -231,17 +158,6 @@ void loop(void) {
     // Defensively exclude unused bits from sregister.
     payload.sreg &= VALID_SREGISTER_MASK;
   }
-
-  //Serial.println(payload.sreg,BIN);
-  // printf(" %4i %4i %4i %4i ",payload.j_RUD,payload.j_RLR,payload.j_LUD,payload.j_LLR);
-  // printf("\n");
-
-  // R2D2: LEFT joystick controls  drive motors,  -255 to 255
-  // note: j_LLR value comes in inversed
-
-  // diagonal drive
-  //joystick[0] = -payload.j_RLR;
-  //joystick[1] = payload.j_RUD;
 
   // quantize joystick values 0-6
 
@@ -323,88 +239,6 @@ void loop(void) {
     mp3.playFileByIndexNumber(4);
   }
 
-  /*
-      // LED Spot Light On / Off
-      if (LUMask & payload.sreg) {
-        if (SpotButton == false) { //  button low to transition so toggle the spot
-          SpotButton = true;
-          if (SpotOn == true) {
-            SpotOn = false;
-            digitalWrite (SpotPin, LOW);
-          }
-          else {
-            SpotOn = true;
-            digitalWrite (SpotPin, HIGH);
-          }
-        }
-      }
-      else {
-        SpotButton = false;
-      }
-
-      // Blue Rotation Light On / Off
-      if (LRMask & payload.sreg) {
-        if (RotButton == false) { //  button low to transition so toggle the spot
-          RotButton = true;
-          if (RotOn == true) {
-            RotOn = false;
-            digitalWrite (RotPin, LOW);
-          }
-          else {
-            RotOn = true;
-            digitalWrite (RotPin, HIGH);
-          }
-        }
-      }
-      else {
-        RotButton = false;
-      }
-
-      // Robot Eyes On / Off
-      if (LLMask & payload.sreg) {
-        if (EyesButton == false) { //  button low to transition so toggle the spot
-          EyesButton = true;
-          if (EyesOn == true) {
-            EyesOn = false;
-            (RightEyeLed, LOW);
-            digitalWrite(LeftEyeLed, LOW);
-          }
-          else {
-            EyesOn = true;
-            digitalWrite(RightEyeLed, HIGH);
-            digitalWrite(LeftEyeLed, HIGH);
-          }
-        }
-      }
-      else {
-        EyesButton = false;
-      }
-
-      // LED Spot Light servo
-      SpotAngle = (payload.j_LLR + 255) / 3; // 0 to 180ish
-      servo1.write(SpotAngle);
-
-
-      // Arm Servo L1 button decreases angle, L2 button increases angle
-      // 0 all up, 180 all down
-      if (L2Mask & payload.sreg) {
-        if (ArmAngle + ArmIncrement < 180) {
-          ArmAngle = ArmAngle + ArmIncrement;
-          servo2.write(ArmAngle);
-        }
-      }
-      if (L1Mask & payload.sreg) {
-        if (ArmAngle - ArmIncrement > 0) {
-          ArmAngle = ArmAngle - ArmIncrement;
-          servo2.write(ArmAngle);
-        }
-      }
-  */
-  // Serial.print(joystick[0]);
-  // Serial.print(' ');
-  // Serial.println(joystick[1]);
-
-
   // left (port) motor
   if (joystick[0] > 0) {      // forward
     analogWrite(LmotorPinF, joystick[0]);
@@ -418,6 +252,7 @@ void loop(void) {
     digitalWrite(LmotorPinF, LOW); // stopped
     digitalWrite(LmotorPinR, LOW);
   }
+  
   // right (starbord) motor
   if (joystick[1] > 0) {      // forward
     analogWrite(RmotorPinF, joystick[1]);
@@ -441,110 +276,4 @@ void loop(void) {
     digitalWrite(HmotorPinF, LOW);
     analogWrite(HmotorPinR, -joystick[2]);
   }
-  /*
-      // shoot disk motors
-      if (R1Mask & payload.sreg) {
-        digitalWrite(SpinMotor, HIGH);
-        digitalWrite(ShootMotor, HIGH);
-      }
-      else {
-        digitalWrite(SpinMotor, LOW);
-        digitalWrite(ShootMotor, LOW);
-      }
-
-      // Hand LEDs
-      if (R2Mask & payload.sreg) {
-        if (lcount == 16) {
-          count += 1;
-          lcount = 0;
-        } else {
-          lcount += 1;
-        }
-        if (count > 3) {
-          count = 1;
-        }
-        //  tone (SpeakerPin, LaserTone);
-        //  LaserTone += 150;
-        //  if (LaserTone > 2000) {
-        //   LaserTone = 60;
-        //}
-        if (count == 1) {
-          digitalWrite(HandLED1, LOW);
-        }
-        else {
-          digitalWrite(HandLED1, HIGH);
-        }
-        if (count == 2) {
-          digitalWrite(HandLED2, LOW);
-        }
-        else {
-          digitalWrite(HandLED2, HIGH);
-        }
-        if (count == 3) {
-          digitalWrite(HandLED3, LOW);
-        }
-        else {
-          digitalWrite(HandLED3, HIGH);
-        }
-        delay (2);
-        //  noTone (SpeakerPin);
-      }
-      else {
-        digitalWrite(HandLED1, LOW);
-        digitalWrite(HandLED2, LOW);
-        digitalWrite(HandLED3, LOW);
-        // LaserTone = 60;
-      }
-       \
-
-      // Play sound tracks from the sound card
-
-
-
-
-
-      /*
-        soundfile = random(79);
-        if (LDMask & payload.sreg) {
-        wtv020sd16p.stopVoice();
-        wtv020sd16p.asyncPlayVoice(soundfile);
-        Serial.print("Now playing sound file #: ");
-        Serial.println(soundfile);
-        }
-        else {
-        if (B1Mask & payload.sreg) {
-          tone (SpeakerPin, 523);
-          delay (10);
-          noTone (SpeakerPin);
-        }
-        else {
-          noTone (SpeakerPin) ;
-        }
-        if (B2Mask & payload.sreg) {
-          tone (SpeakerPin, 587);
-          delay (10);
-          noTone (SpeakerPin);
-        }
-        else {
-          noTone (SpeakerPin) ;
-        }
-        if (B3Mask & payload.sreg) {
-          tone (SpeakerPin, 659);
-          delay (10);
-          noTone (SpeakerPin);
-        }
-        else {
-          noTone (SpeakerPin) ;
-        }
-        if (B4Mask & payload.sreg) {
-          tone (SpeakerPin, 698);
-          delay (10);
-          noTone (SpeakerPin);
-        }
-        else {
-          noTone (SpeakerPin) ;
-        }
-        noTone (SpeakerPin);
-        }
-  */
 }
